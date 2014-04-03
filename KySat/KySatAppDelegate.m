db89b2106e1f1ab6223bf2ccb89e12271ff63de8
Matthew Fahrbach <matthew.fahrbach@gmail.com>
2014-04-02 21:58:30 -0400

    Only shows the orbit of the ISS.

--- KySat/KySatAppDelegate.m
+++ KySat/KySatAppDelegate.m
@@ -8,6 +8,9 @@
 //  Resources:
 //  - http://mobiforge.com/design-development/using-background-fetch-ios
 //  - http://stackoverflow.com/questions/11757107/ios-how-to-handle-images
+//  - http://stackoverflow.com/questions/15702608/faster-way-to-load-an-image-from-a-url-and-display-it-in-an-iphone-app
+//  - https://github.com/nicklockwood/AsyncImageView
+//  - http://www.heavens-above.com/orbit.aspx?satid=25544&lat=0&lng=0&loc=Unspecified&alt=0&tz=UCT
 
 #import "KySatAppDelegate.h"
 #import "KySatViewController.h"
@@ -20,94 +23,6 @@
 @end
 
 @implementation KySatAppDelegate
-
--(void)               application:(UIApplication *)application
-performFetchWithCompletionHandler:
-(void (^)(UIBackgroundFetchResult))completionHandler {
-    
-    //---do background fetch here---
-    // You have up to 30 seconds to perform the fetch
-    
-    /*
-     BOOL downloadSuccessful = YES;
-     
-     if (downloadSuccessful) {
-     //---set the flag that data is successfully downloaded---
-     completionHandler(UIBackgroundFetchResultNewData);
-     } else {
-     //---set the flag that download is not successful---
-     completionHandler(UIBackgroundFetchResultFailed);
-     }
-     */
-    
-    NSLog(@"Background fetch started...");
-    NSString *urlString = [NSString stringWithFormat:
-                           @"http://api.openweathermap.org/data/2.5/weather?q=%@",
-                           @"Lexington,USA"];
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    [[session dataTaskWithURL:[NSURL URLWithString:urlString]
-            completionHandler:^(NSData *data,
-                                NSURLResponse *response,
-                                NSError *error) {
-                NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*) response;
-                if (!error && httpResp.statusCode == 200) {
-                    //---print out the result obtained---
-                    NSString *result =
-                    [[NSString alloc] initWithBytes:[data bytes]
-                                             length:[data length]
-                                           encoding:NSUTF8StringEncoding];
-                    NSLog(@"%@", result);
-                    
-                    //---parse the JSON result---
-                    [self parseJSONData:data];
-                    
-                    //---update the UIViewController---
-                    KySatViewController *vc =
-                    (KySatViewController *)
-                    [[[UIApplication sharedApplication] keyWindow]
-                     rootViewController];
-                    dispatch_sync(dispatch_get_main_queue(), ^{
-                        vc.lblStatus.text = self.temperature;
-                    });
-                    
-                    completionHandler(UIBackgroundFetchResultNewData);
-                    NSLog(@"Background fetch completed...");
-                } else {
-                    NSLog(@"%@", error.description);
-                    completionHandler(UIBackgroundFetchResultFailed);
-                    NSLog(@"Background fetch Failed...");
-                }
-            }
-      ] resume
-     ];
-}
-
-- (void)parseJSONData:(NSData *)data {
-    NSError *error;
-    NSDictionary *parsedJSONData =
-    [NSJSONSerialization JSONObjectWithData:data
-                                    options:kNilOptions
-                                      error:&error];
-    NSDictionary *main = [parsedJSONData objectForKey:@"main"];
-    
-    //---temperature in Kelvin---
-    NSString *temp = [main valueForKey:@"temp"];
-    
-    //---convert temperature to Celcius---
-    float temperature = [temp floatValue] - 273;
-    
-    //---get current time---
-    NSDate *date = [NSDate date];
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setDateFormat:@"HH:mm:ss"];
-    
-    NSString *timeString = [formatter stringFromDate:date];
-    
-    self.temperature = [NSString stringWithFormat:
-                        @"%f degrees Celsius, fetched at %@",
-                        temperature, timeString];
-}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -134,8 +49,13 @@ performFetchWithCompletionHandler:
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
-    printf("app entered foreground\n");
+    printf("enter function\n");
     // MCF: Attempting to update the image whenever the app will enter the foreground
     // http://stackoverflow.com/questions/15702608/faster-way-to-load-an-image-from-a-url-and-display-it-in-an-iphone-app
     dispatch_queue_t q = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
@@ -155,11 +75,7 @@ performFetchWithCompletionHandler:
             vc.imageViewStatus.image = img;
         });
     });
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    printf("exit function\n");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
