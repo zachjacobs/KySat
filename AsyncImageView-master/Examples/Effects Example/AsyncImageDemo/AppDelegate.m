2fb0b44a70f2ff96b4df061c2f0e1045bd8ada3d
Matthew Fahrbach <matthew.fahrbach@gmail.com>
2014-04-16 14:23:42 -0400

    Added necessary Xcode files back to the Github page

---
+++ AsyncImageView-master/Examples/Effects Example/AsyncImageDemo/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  AsyncImageDemo
+//
+//  Created by Nick Lockwood on 17/10/2011.
+//  Copyright (c) 2011 Charcoal Design. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [_window makeKeyAndVisible];
+    return YES;
+}
+
+@end
---
+++ AsyncImageView-master/Examples/Effects Example/AsyncImageDemo/ImageViewController.m
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.m
+//  AsyncImageDemo
+//
+//  Created by Nick Lockwood on 17/10/2011.
+//  Copyright (c) 2011 Charcoal Design. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@implementation ImageViewController
+
+@end
